251f396e502aaea757f176820c449eb4c3993851
Porky623 <philip623.derek@gmail.com>
2019-08-20 18:38:06 -0400

    Announcement system added in About page

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ router.use('/', require('./app/routes/officer').router);
 
 router.use('/', require('./app/routes/contest'));
 
+router.use('/', require('./app/routes/announcement'));
+
 router.use('/test', require('./app/routes/test'));
 
 module.exports = router;
