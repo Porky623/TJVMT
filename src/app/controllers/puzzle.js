76ebdeb4c750265fbc52d4273b52905508de7dcb
Bryan Lu <2020blu@tjhsst.edu>
2020-01-31 16:06:18 -0500

    added city pages

--- src/app/controllers/puzzle.js
+++ src/app/controllers/puzzle.js
@@ -20,3 +20,24 @@ exports.load_map = async(req,res) => {
     };
     res.render('puzzle_map');
 };
+
+exports.load_tree = async(req,res) => {
+    res.locals.metaTags = {
+        title: 'Abstracton',
+    };
+    res.render('puzzle_tree');
+};
+
+exports.load_rings = async(req,res) => {
+    res.locals.metaTags = {
+        title: 'Deltopolis',
+    };
+    res.render('puzzle_rings');
+};
+
+exports.load_final = async(req,res) => {
+    res.locals.metaTags = {
+        title: 'Capital',
+    };
+    res.render('puzzle_final');
+};
--- src/app/routes/puzzle.js
+++ src/app/routes/puzzle.js
@@ -8,4 +8,10 @@ router.get('/rules', puzzleController.load_rules);
 
 router.get('/map', puzzleController.load_map);
 
+router.get('/abstracton', puzzleController.load_tree);
+
+router.get('/deltopolis', puzzleController.load_rings);
+
+router.get('/capital', puzzleController.load_final);
+
 module.exports = router;
