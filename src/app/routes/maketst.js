5f1c2dab1cc4431898bab88df858a71834c8d1f1
clutchnoob <67919245+clutchnoob@users.noreply.github.com>
2020-07-11 18:16:07 -0400

    Add files via upload

---
+++ src/app/routes/maketst.js
@@ -0,0 +1,14 @@
+// libraries
+const express = require('express');
+const router = express.Router();
+const marked = require('marked');
+
+// models
+const TSTModel = require('../models/tstmodel');
+const officerCheck = require('./officer').officerCheck;
+
+router.post('/taketst', async (_req, res) => {
+    
+},  
+
+module.exports = router; 
