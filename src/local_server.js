4163b3f9c5cda1dbbccf2a92011cfa3ac530299e
pmathur <pranavmathur001@gmail.com>
2020-09-28 23:59:57 -0400

    Fixed server.js and local_server.js

--- src/local_server.js
+++ src/local_server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
-const cookieSession = require('cookie-session');
+const session = require('express-session');
 const passport = require('passport');
 const passportSetup = require('./config/passport-setup');
 const keys = require('./config/keys');
